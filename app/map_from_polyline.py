b83aded909daae23316320b49563bccca32dd0bc
Neo <maksym.blazhko.pydev@gmail.com>
2024-02-07 14:03:14 +0100

    clear dataframe

--- app/map_from_polyline.py
+++ app/map_from_polyline.py
@@ -1,10 +1,13 @@
+import os
 from pprint import pprint
-
+import json
 import polyline
 import folium
 
+with open(os.path.join("..", "tests", "results.json"), "r") as f:
+    results = json.load(f)
 
-encoded_polyline = "{y}cH_qzn@na@rw@tVuvA`QuhCjwAqjLlRksFz`AkuA|~AuuAns@o~Bl`Asy@t{@qnGfD_\\tk@moKia@icEin@apA`w@elBry@cvC}GotBnZyoKriA}`F}N}|FylAc_L{`BywDyi@{fFnBayLnk@ajFgAabEvIe}Ab~@cmBnrAahCbY_yCkCiqDpqCkdAxnFyeHdkBs|Ck^csByeA{|FufAq}LyN}_JppC{}JjvCyhIbpCgbEv{BowDn~@aeC~eB|A~zD_hHneB_yEfu@seCqC_}B{Z{`Eor@izGzaBwwFtTmnBrfAgcAhaCo|ExbC_rFfb@otG~xBezFrq@}uHiKksBtuBcDh{@bBbq@_vBthBs{AhjC~OdsA~bC~`Cvx@to@jqA|rDhSdcKjrJrrEhcE~dDxi@d_FiAl~AsI~zChu@rxA}eGnkBe`D|r@etEp_BalDzlAcxHdcDmuFup@a`Jr_BqgNb]_wHu`@{pJnb@}pC_b@ahBtHwxD`h@eoUuhBcnEaYimEqSovCaV_nG~`BqkDeNmbF`h@gnF{x@_~F}dDyyGubAw~DuhBc{DkiBsdGoe@goJyl@orD_AmiFsoAsbEurD{jIau@g{JywBkpM{fAkdJmH__E|kBg{D|aAizHnbBgnI}Co_JvhAgvEqEoeLc_Ah_@}Ola@kRi[tt@gqA}OqxAfn@hw@~]|eCveAntAd{D{hA`yBnGrq@~QqFwdAhyBwzCtiEacBdaCoMnsCoi@vt@kpAjlA{r@b~Cp_@pxAm}BpcBqpAbhC|gAj|CloExeGr_CpwAh}AhpBbGblDuoNfcBqjB~nDmbBxjBisJ`_CgzCbc@{fArzAmAxvDuq@zyCmZj{IjuJ`fFz{H`uI|uCxjC`qBvkBffBbgDxaIuLh|IdkAhtH`yCtdB~dCshAffCz{@dtH`kBhcFzoB|iH`fF~`D|wBrlB~|FbrBdeEzzBp`BpwBnkBhsEbjCnsDvBf{DsnBt~DwVlvCopApeEfHtvHvxBbaCvyDtpA~yExiD`sCnnGvgApuDyv@~~AkaAbuAdQlbFbfE|uBdmG`{MpkIbtIddHlgCtzEv`DjoAmC~o@bkAbp@zuCnx@h{Fff@viFp~Bzp@d~@}A~eClJ`v@oa@noOe}Dzfe@kaEbq[{gEnfUowBnrG|jAjxI`oBthZfuBtlKzYf_LfPhmIu~B~yK~_@hoJkPzzJnH`gN~ThlEbs@kJduArpAxuAeKuf@vhF{~@toBjFhxDha@fkHt]zqDxMnjDd^dsI"
+encoded_polyline = results[9]["polyline"]
 decoded_polyline = polyline.decode(encoded_polyline)
 map_route = folium.Map(
     location=[decoded_polyline[0][0], decoded_polyline[0][1]], zoom_start=10
