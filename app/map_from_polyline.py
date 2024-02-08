bfce5f07d130ba18dde50cdd6b2fc1fbe1d9f9d9
Neo <maksym.blazhko.pydev@gmail.com>
2024-02-08 11:10:18 +0100

    add creating maps from results

--- app/map_from_polyline.py
+++ app/map_from_polyline.py
@@ -1,21 +1,39 @@
 import os
+from concurrent.futures.thread import ThreadPoolExecutor
 from pprint import pprint
 import json
 import polyline
 import folium
 
-with open(os.path.join("..", "tests", "results.json"), "r") as f:
-    results = json.load(f)
 
-encoded_polyline = results[9]["polyline"]
-decoded_polyline = polyline.decode(encoded_polyline)
-map_route = folium.Map(
-    location=[decoded_polyline[0][0], decoded_polyline[0][1]], zoom_start=10
-)
+def make_map(poly):
+    encoded_polyline = poly["polyline"]
+    decoded_polyline = polyline.decode(encoded_polyline)
 
-folium.PolyLine(locations=decoded_polyline, color='blue').add_to(map_route)
+    map_route = folium.Map(
+        location=[decoded_polyline[0][0], decoded_polyline[0][1]], zoom_start=20
+    )
+    folium.Marker(
+        location=[decoded_polyline[0][0], decoded_polyline[0][1]]
+    ).add_to(map_route)
+    folium.Marker(
+        location=[decoded_polyline[-1][0], decoded_polyline[-1][1]]
+    ).add_to(map_route)
+    folium.PolyLine(locations=decoded_polyline, color="blue").add_to(map_route)
+
+    file_name = f"{poly["start_address"]}-{poly["end_address"]}.html".replace(
+        " ", ""
+    )
+    save_path = os.path.join("..", "maps", file_name)
+    map_route.save(save_path)
+
+
+def create_map_from_polyline(polylines):
+    with ThreadPoolExecutor() as executor:
+        executor.map(make_map, polylines)
 
 
 if __name__ == "__main__":
-    map_route.save('here_route_map.html')
-    pprint(decoded_polyline)
+    with open(os.path.join("..", "tests", "results.json"), "r") as f:
+        results = json.load(f)
+    create_map_from_polyline(results)
